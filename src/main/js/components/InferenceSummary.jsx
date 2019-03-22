3f773b3dcff7a3d6a147b39f92d683acf10104c3
Iain Monro <iamonro@gmail.com>
2019-03-22 22:14:04 +0000

    Shut react up about keys for summaries with more than 2 premises.

--- src/main/js/components/InferenceSummary.jsx
+++ src/main/js/components/InferenceSummary.jsx
@@ -1,24 +1,33 @@
 import React from "react";
-import _ from "lodash";
 import {Expression} from "./Expression";
 
 export class InferenceSummary extends React.Component {
-  renderSinglePremise(premise) {
-    return <div>Suppose {premise}.</div>;
-  }
-  renderMultiplePremises(premises) {
-    let initialPremises = _.flatMap(premises.slice(0, -1), p => [p, <span>, </span>]).slice(0, -1);
-    let lastPremise =  premises.slice(-1)[0];
-    return <div>Suppose {initialPremises} and {lastPremise}.</div>;
+  renderPremises(premises, createPremiseElement) {
+    let premiseElements = premises.map(createPremiseElement);
+    let allElements = [];
+    if (premiseElements.length > 0) {
+      allElements.push(<React.Fragment>Suppose </React.Fragment>)
+    }
+    while (premiseElements.length > 2) {
+      allElements.push(premiseElements.shift());
+      allElements.push(<React.Fragment>, </React.Fragment>)
+    }
+    if (premiseElements.length > 1) {
+      allElements.push(premiseElements.shift());
+      allElements.push(<React.Fragment> and </React.Fragment>)
+    }
+    if (premiseElements.length > 0) {
+      allElements.push(premiseElements.shift());
+      allElements.push(<React.Fragment>.</React.Fragment>)
+    }
+    return allElements.map((e, i) => <React.Fragment key={i}>{e}</React.Fragment>)
   }
   render() {
     let {inference, createPremiseElement} = this.props;
     createPremiseElement = createPremiseElement || (p => <Expression key={p.serialize()} expression={p} boundVariableLists={[]}/>);
-    let premiseElements = inference.premises.map(createPremiseElement);
-    let premiseElement = premiseElements.length > 0 && (premiseElements.length > 1 ? this.renderMultiplePremises(premiseElements) : this.renderSinglePremise(premiseElements[0]));
     return <div className={this.props.className}>
-      {premiseElement}
-      <div>{premiseElements.length > 0 && "Then "}<Expression expression={inference.conclusion} boundVariableLists={[]}/>.</div>
+      {this.renderPremises(inference.premises, createPremiseElement)}
+      <div>{inference.premises.length > 0 && "Then "}<Expression expression={inference.conclusion} boundVariableLists={[]}/>.</div>
     </div>
   }
 };
