56c11c0d6304560ba116e5f9173ea703c46d1672
Iain Monro <iamonro@gmail.com>
2019-03-25 12:08:22 +0000

    Fix broken test

--- src/test/scala/net/prover/viewmodel/HtmlHelperSpec.scala
+++ src/test/scala/net/prover/viewmodel/HtmlHelperSpec.scala
@@ -3,10 +3,12 @@ package net.prover.viewmodel
 import net.prover.model.HtmlHelper
 import org.specs2.mutable.Specification
 
+import scala.xml.Text
+
 class HtmlHelperSpec extends Specification {
   "HtmlHelper" should {
     "convert subscripts and superscripts into HTML" in {
-      HtmlHelper.format("a_b c^d").toString mustEqual "<span>a<sub>b</sub> c<sup>d</sup></span>"
+      HtmlHelper.format("a_b c^d") mustEqual Seq(Text("a"), <sub>b</sub>, Text(" c"), <sup>d</sup>, Text(""))
     }
   }
 }
