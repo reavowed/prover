487f3492de356cd9c0f2096b96a6e8cdfd939529
Iain Monro <iamonro@gmail.com>
2019-03-26 21:54:22 +0000

    Popovers for assumptions and scoped variables

--- src/main/js/components/steps/DeductionStep.jsx
+++ src/main/js/components/steps/DeductionStep.jsx
@@ -1,5 +1,8 @@
 import React from "react";
-import {HighlightableExpression} from "../Expression";
+import Popover from "react-bootstrap/Popover";
+import {Expression, HighlightableExpression} from "../Expression";
+import {FlexRow} from "../FlexRow";
+import {DeleteStepButton} from "./DeleteStepButton";
 import {ProofLine} from "./ProofLine";
 import {Steps} from "./Steps";
 
@@ -8,8 +11,13 @@ export class DeductionStep extends React.Component {
     let {step, path, additionalReferences, ...otherProps} = this.props;
     let reference = path.join(".");
     let referencesForLastStep = [...additionalReferences, reference];
+    const popover = (
+      <Popover title={<FlexRow><FlexRow.Grow>Assumption</FlexRow.Grow><DeleteStepButton path={path} {...otherProps}/></FlexRow>}>
+        <Expression expression={step.assumption} boundVariableLists={this.props.boundVariableLists}/>
+      </Popover>
+    );
     return <>
-      <ProofLine>Assume <HighlightableExpression expression={step.assumption} boundVariableLists={this.props.boundVariableLists} references={[...additionalReferences, reference, reference + "a"]} {...otherProps}/>.</ProofLine>
+      <ProofLine popover={popover}>Assume <HighlightableExpression expression={step.assumption} boundVariableLists={this.props.boundVariableLists} references={[...additionalReferences, reference, reference + "a"]} {...otherProps}/>.</ProofLine>
       <Steps.Children steps={step.substeps} path={path} referencesForLastStep={referencesForLastStep} {...otherProps} />
     </>;
   }
--- src/main/js/components/steps/ScopedVariableStep.jsx
+++ src/main/js/components/steps/ScopedVariableStep.jsx
@@ -1,11 +1,26 @@
 import React from "react";
+import Popover from "react-bootstrap/Popover";
+import {Expression} from "../Expression";
+import {FlexRow} from "../FlexRow";
+import {DeleteStepButton} from "./DeleteStepButton";
+import {ProofLine} from "./ProofLine";
 import {Steps} from "./Steps";
 
 export class ScopedVariableStep extends React.Component {
   render() {
     let {step, path, boundVariableLists, additionalReferences, ...otherProps} = this.props;
-    let reference = path.join(".");
-    let referencesForLastStep = [...additionalReferences, reference];
-    return <Steps steps={step.substeps} path={path} boundVariableLists={[[step.variableName], ...boundVariableLists]} referencesForLastStep={referencesForLastStep} {...otherProps} />
+    if (step.substeps.length) {
+      let reference = path.join(".");
+      let referencesForLastStep = [...additionalReferences, reference];
+      return <Steps steps={step.substeps} path={path} boundVariableLists={[[step.variableName], ...boundVariableLists]}
+                    referencesForLastStep={referencesForLastStep} {...otherProps} />
+    } else {
+      const popover = (
+        <Popover title={<FlexRow><FlexRow.Grow>Scoped variable</FlexRow.Grow><DeleteStepButton path={path} {...otherProps}/></FlexRow>}/>
+      );
+      return <ProofLine popover={popover}>
+        Take any <Expression expression={{textForHtml: () => step.variableName}}/>.
+      </ProofLine>
+    }
   }
 }
