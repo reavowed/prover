e7cda5c98b4bdb802953337999bc300c12c1e9a7
Iain Monro <iamonro@gmail.com>
2019-03-27 08:23:31 +0000

    Move bound variable modal into assertion step popover

--- src/main/js/components/steps/AssertionStep.jsx
+++ src/main/js/components/steps/AssertionStep.jsx
@@ -26,10 +26,43 @@ export class AssertionStep extends React.Component {
   constructor(...args) {
     super(...args);
     this.state = {
-      premiseOptions: {},
-      boundVariableLocation: null,
-      boundVariableName: ''
+      showingModal: false
     };
+  }
+  onModalTransition = (showingModal) => {
+    this.setState({showingModal});
+  };
+
+
+  render() {
+    let {step, path, additionalReferences, ...otherProps} = this.props;
+    let reference = path.join(".");
+
+    return <ProofLine premiseReferences={step.referencedLines}
+                 path={path}
+                 popover={<AssertionStep.Popover step={step} path={path} premiseOptions={this.state.premiseOptions} onTransition={this.onModalTransition} {...otherProps}/>}
+                 blockHide={this.state.showingModal}
+                 incomplete={_.some(step.premises, "incomplete")}
+                 {...otherProps}
+      >
+        Then <HighlightableExpression statement={step.statement}
+                                      boundVariableLists={this.props.boundVariableLists}
+                                      references={[...additionalReferences, reference]}
+                                      {...otherProps}
+      />.
+    </ProofLine>;
+  }
+}
+
+AssertionStep.Popover = class extends React.Component {
+  constructor(...args) {
+    super(...args);
+    this.state = {
+      premiseOptions: {}
+    };
+    if (_.some(this.props.step.premises, "incomplete")) {
+      this.fetchOptions();
+    }
   }
 
   fetchOptions = () => {
@@ -45,71 +78,13 @@ export class AssertionStep extends React.Component {
       });
   };
 
-  showBoundVariableModal = () => {
-    return this.state.boundVariableLocation != null;
-  };
-  hideBoundVariableModal = () => {
-    setTimeout(() => this.setState({boundVariableLocation: null}), 0);
-  };
-  updateBoundVariableName = (event) => {
-    this.setState({boundVariableName: event.target.value});
-  };
-  saveBoundVariable = () => {
-    const {boundVariableIndex, boundVariablePath, premisePath} = this.state.boundVariableLocation;
-    this.props.fetchForStep(this.props.path, `premises/${premisePath.join(".")}/statement/${boundVariablePath.join(".")}/boundVariables/${boundVariableIndex}/`, {
-      method: "PUT",
-      body: this.state.boundVariableName
-    })
-      .then(this.props.updateTheorem)
-      .then(this.hideBoundVariableModal);
-  };
-
-  render() {
-    let {step, path, additionalReferences, ...otherProps} = this.props;
-    let reference = path.join(".");
-    const boundVariableModal = <BoundVariableModal show={this.showBoundVariableModal()}
-                                                   onHide={this.hideBoundVariableModal}
-                                                   title="Edit bound variable"
-                                                   value={this.state.boundVariableName}
-                                                   onChange={this.updateBoundVariableName}
-                                                   onSave={this.saveBoundVariable}/>;
-
-    return <>
-      <ProofLine premiseReferences={step.referencedLines}
-                 path={path}
-                 popover={<AssertionStep.Popover step={step} path={path} premiseOptions={this.state.premiseOptions} fetchOptions={this.fetchOptions} {...otherProps}/>}
-                 onShowPopover={this.fetchOptions}
-                 blockHide={this.showBoundVariableModal()}
-                 incomplete={_.some(step.premises, "incomplete")}
-                 {...otherProps}
-      >
-        Then <HighlightableExpression statement={step.statement}
-                                      boundVariableLists={this.props.boundVariableLists}
-                                      references={[...additionalReferences, reference]}
-                                      {...otherProps}
-      />.
-      </ProofLine>
-      {boundVariableModal}
-    </>;
-  }
-}
-
-AssertionStep.Popover = class extends React.Component {
-  constructor(...args) {
-    super(...args);
-    this.state = {
-      hideOptions: false
-    };
-  }
-
   fetchAndUpdateOptions = (fetchPath, fetchOptions) => {
     this.props.fetchForStep(this.props.path, fetchPath, fetchOptions)
       .then(response => {
-        this.setState({hideOptions: true});
+        this.setState({premiseOptions: {}});
         this.props.updateTheorem(response);
       })
-      .then(this.props.fetchOptions)
-      .then(() => this.setState({hideOptions: false}));
+      .then(this.fetchOptions)
   };
 
   applyExpansion = (premisePath, expansionId) => {
@@ -129,38 +104,60 @@ AssertionStep.Popover = class extends React.Component {
     this.fetchAndUpdateOptions(`premises/${premisePath.join(".")}/`, {method: "DELETE"});
   };
   addTarget = (premisePath) => {
-    this.props.fetchAndUpdateOptions(`premises/${premisePath.join(".")}/target`, {
+    this.fetchAndUpdateOptions(`premises/${premisePath.join(".")}/target`, {
       method: "POST"
     });
   };
 
-  renderPremise(premise, path, boundVariableLists) {
+  isShowingBoundVariableModal = () => {
+    return this.state.boundVariableLocation != null;
+  };
+  showBoundVariableModal = (boundVariableName, boundVariableIndex, boundVariablePath, premisePath) => {
+    this.setState({
+      boundVariableLocation: {boundVariableIndex, boundVariablePath, premisePath},
+      boundVariableName: boundVariableName
+    });
+    this.props.onTransition(true);
+  };
+  hideBoundVariableModal = () => {
+    setTimeout(() => this.setState({boundVariableLocation: null}), 0);
+    this.props.onTransition(false);
+  };
+  updateBoundVariableName = (event) => {
+    this.setState({boundVariableName: event.target.value});
+  };
+  saveBoundVariable = () => {
+    const {boundVariableIndex, boundVariablePath, premisePath} = this.state.boundVariableLocation;
+    this.props.fetchForStep(this.props.path, `premises/${premisePath.join(".")}/statement/${boundVariablePath.join(".")}/boundVariables/${boundVariableIndex}/`, {
+      method: "PUT",
+      body: this.state.boundVariableName
+    })
+      .then(this.props.updateTheorem)
+      .then(this.hideBoundVariableModal);
+  };
 
+  renderPremise(premise, premisePath, boundVariableLists) {
     switch (premise.type) {
       case "pending":
-        const options = !this.state.hideOptions && _.find(this.props.premiseOptions, option => _.isEqual(option.path, path)) || {};
+        const options = !this.state.hideOptions && _.find(this.state.premiseOptions, option => _.isEqual(option.path, premisePath)) || {};
         const wrapEditableBoundVariable = (boundVariableContent, boundVariableName, boundVariableIndex, boundVariablePath) => {
-          const startEditingBoundVariable = () => {
-            this.setState({
-              boundVariableLocation: {boundVariableIndex, boundVariablePath, premisePath: path},
-              boundVariableName: boundVariableName
-            });
-          };
-          return <EditableBoundVariable onClick={startEditingBoundVariable}>{boundVariableContent}</EditableBoundVariable>;
+          return <EditableBoundVariable onClick={() => this.showBoundVariableModal(boundVariableName, boundVariableIndex, boundVariablePath, premisePath)}>
+            {boundVariableContent}
+          </EditableBoundVariable>;
         };
 
         return <FlexRow>
           <FlexRow.Grow><Expression expression={premise.statement} boundVariableLists={boundVariableLists} wrapBoundVariable={wrapEditableBoundVariable}/></FlexRow.Grow>
-          <Button size="sm" className="ml-1" onClick={() => this.addTarget(path)}>Target</Button>
+          <Button size="sm" className="ml-1" onClick={() => this.addTarget(premisePath)}>Target</Button>
           {options.expansions && options.expansions.length > 0 && <DropdownButton title="Expansions" size="sm" className="ml-1">
-            {options.expansions.map(e => <Dropdown.Item key={e.id} onClick={() => this.applyExpansion(path, e.id)}>{e.name}</Dropdown.Item>)}
+            {options.expansions.map(e => <Dropdown.Item key={e.id} onClick={() => this.applyExpansion(premisePath, e.id)}>{e.name}</Dropdown.Item>)}
           </DropdownButton>}
           {options.quick && options.quick.length > 0 && <DropdownButton title="Quick" size="sm" className="ml-1">
             {options.quick.map(e => {
               const content = _.countBy(options.quick, "inference.id")[e.inference.id] > 1 ?
                 <>{e.inference.name} - <Expression expression={e.target} boundVariableLists={boundVariableLists}/></> :
                 e.inference.name;
-              return <Dropdown.Item key={e.id + " " + e.target.serialize()} onClick={() => this.applyQuick(path, e.inference.id, e.target)}>{content}</Dropdown.Item>
+              return <Dropdown.Item key={e.id + " " + e.target.serialize()} onClick={() => this.applyQuick(premisePath, e.inference.id, e.target)}>{content}</Dropdown.Item>
             })}
           </DropdownButton>}
         </FlexRow>;
@@ -168,10 +165,10 @@ AssertionStep.Popover = class extends React.Component {
         return <>
           <FlexRow>
             <FlexRow.Grow><Expression expression={premise.statement} boundVariableLists={boundVariableLists}/></FlexRow.Grow>
-            <Button size="sm" className="ml-1" onClick={() => this.deletePremise(path)}><span className="fas fa-ban"/></Button>
+            <Button size="sm" className="ml-1" onClick={() => this.deletePremise(premisePath)}><span className="fas fa-ban"/></Button>
           </FlexRow>
           <PremiseChildren>
-            {premise.premises.map((p, i) => this.renderPremise(p, [...path, i], boundVariableLists))}
+            {premise.premises.map((p, i) => this.renderPremise(p, [...premisePath, i], boundVariableLists))}
           </PremiseChildren>
         </>;
       case "given":
@@ -184,14 +181,24 @@ AssertionStep.Popover = class extends React.Component {
 
   render() {
     const {step, path, boundVariableLists, innerRef} = this.props;
-    const inference = step.inference || step.inferenceApplication.inference;
-    return <Popover ref={innerRef} title={<FlexRow><FlexRow.Grow><a href={inference.key.url}>{inference.name}</a></FlexRow.Grow>{path && <DeleteStepButton path={path} {...this.props}/>}</FlexRow>}  {...this.props}>
+    const inference = step.inference || step.inferenceApplication.inference;;
+    const boundVariableModal = <BoundVariableModal show={this.isShowingBoundVariableModal()}
+                                                   onHide={this.hideBoundVariableModal}
+                                                   title="Edit bound variable"
+                                                   value={this.state.boundVariableName}
+                                                   onChange={this.updateBoundVariableName}
+                                                   onSave={this.saveBoundVariable}/>;
+
+    return <>
+      <Popover ref={innerRef} title={<FlexRow><FlexRow.Grow><a href={inference.key.url}>{inference.name}</a></FlexRow.Grow>{path && <DeleteStepButton path={path} {...this.props}/>}</FlexRow>}  {...this.props}>
         <InferenceSummary inference={inference} />
         {step.premises && <>
           <hr/>
           <div><strong>Premises</strong></div>
           {step.premises.map((p, i) => <React.Fragment key={p.statement.serialize()}>{this.renderPremise(p, [i], boundVariableLists)}</React.Fragment>)}
         </>}
-      </Popover>;
-  }Ta
+      </Popover>
+      {boundVariableModal}
+    </>;
+  }
 }
