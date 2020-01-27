fd4da61b1e0ca613d87eb153e9e48fbcf72931b8
Iain Monro <iamonro@gmail.com>
2020-01-27 19:02:25 +0000

    Display error messages when adding chapter entries

--- src/main/js/components/helpers/ObjectInputWithSimpleForm.jsx
+++ src/main/js/components/helpers/ObjectInputWithSimpleForm.jsx
@@ -1,4 +1,6 @@
+import _ from "lodash";
 import React, {createRef} from "react";
+import Alert from "react-bootstrap/Alert";
 import Button from "react-bootstrap/Button";
 import SimpleControlGroup from "./SimpleControlGroup";
 
@@ -9,6 +11,7 @@ export default class ObjectInputWithSimpleForm extends React.Component {
     this.firstInputRef = createRef();
     this.state = {
       object: _.fromPairs(_.map(props.values, v => [v.key, v.initialValue || ""])),
+      errorMessage: null,
       saving: false
     };
   }
@@ -20,18 +23,29 @@ export default class ObjectInputWithSimpleForm extends React.Component {
     }
   }
 
+  onError = (message) => {
+    console.log(message);
+    if (_.isObject(message)) {
+      if (message.message)
+        message = message.message;
+      else {
+        message = "Unknown error"
+      }
+    }
+    this.setState({errorMessage: message});
+  };
+
   save = () => {
-    this.setState({saving: true}, () => {
-      this.props.save({...this.state.object})
-        .then(this.props.onCancel)
-        .catch(() => {})
-        .then(() => this.setState({saving: false}));
-    });
+    this.setStatePromise({saving: true})
+      .then(() => this.props.save({...this.state.object}))
+      .then(this.props.onCancel)
+      .catch(this.onError)
+      .then(() => this.setStatePromise({saving: false}));
   };
 
   render() {
     const {description, values, onCancel} = this.props;
-    const {saving, object} = this.state;
+    const {saving, object, errorMessage} = this.state;
 
     return <div ref={this.ref}>
       <h4>Add {description}</h4>
@@ -41,6 +55,7 @@ export default class ObjectInputWithSimpleForm extends React.Component {
       })}
       <Button size="sm" onClick={this.save} disabled={saving}>Save {description}</Button>
       <Button size="sm" className="ml-2" variant="danger" onClick={onCancel} disabled={saving}>Cancel</Button>
+      {errorMessage && <Alert variant="danger" className="mt-2" onClose={() => this.onError(null)} dismissible>{errorMessage}</Alert>}
     </div>
   }
 }
