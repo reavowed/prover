eb24a00b3f920a842f3c3c7cccb94087cb6d13a5
Iain Monro <iamonro@gmail.com>
2020-03-20 19:54:39 +0000

    Better search parsing for inferences

--- src/main/scala/net/prover/controllers/InferenceSearch.scala
+++ src/main/scala/net/prover/controllers/InferenceSearch.scala
@@ -7,7 +7,25 @@ trait InferenceSearch {
     inferences.filter(inferenceFilter(searchText))
   }
   def inferenceFilter(searchText: String): Inference => Boolean = {
-    val searchWords = searchText.toLowerCase().splitByWhitespace()
-    (inference: Inference) => searchWords.forall(inference.name.toLowerCase.contains)
+    val searchWords = searchText.toLowerCase().splitByWhitespace().filter(_.nonEmpty)
+    (inference: Inference) => matchWords(searchWords, inference.name.toLowerCase().splitByWhitespace().filter(_.nonEmpty))
+  }
+  private def matchWords(searchWords: Seq[String], titleWords: Seq[String]): Boolean = {
+    if (searchWords.isEmpty)
+      true
+    else if (titleWords.isEmpty)
+      false
+    else if (searchWords.head.isEmpty)
+      matchWords(searchWords.tail, titleWords)
+    else if (titleWords.head.isEmpty)
+      matchWords(searchWords, titleWords.tail)
+    else if (searchWords.head(0) == titleWords.head(0)) {
+      matchWords(
+        searchWords.head.tail +: searchWords.tail,
+        titleWords.head.tail +: titleWords.tail) ||
+      matchWords(searchWords, titleWords.tail)
+    } else {
+      matchWords(searchWords, titleWords.tail)
+    }
   }
 }
