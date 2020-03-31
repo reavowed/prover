93f16e14fc0de9e740af100027bb209d45b6f230
Iain Monro <iamonro@gmail.com>
2020-03-31 13:59:53 +0100

    Strikethrough incomplete inferences instead of highlighting them in red

--- src/main/js/components/pages/theorem/steps/components/InferenceLink.jsx
+++ src/main/js/components/pages/theorem/steps/components/InferenceLink.jsx
@@ -7,10 +7,11 @@ import {InferenceSummary} from "../../../../InferenceSummary";
 
 export const InferenceLink = ({inference}) => {
   return <HashParamsContext.Consumer>{params => {
-    const shouldHighlight = !inference.isComplete || _.includes(params.inferencesToHighlight, inference.id);
+    const shouldHighlight = _.includes(params.inferencesToHighlight, inference.id);
+    const shouldStrikethrough = !inference.isComplete;
     return <OverlayTrigger placement="bottom"
                            overlay={<Popover title={inference.name}><InferenceSummary inference={inference}/></Popover>}>
-      <a href={inference.url} className="text-uppercase" style={{"fontFamily": "monospace", "color": shouldHighlight ? "red" : "#6c757d"}} tabIndex={-1}>{inference.title}</a>
+      <a href={inference.url} className="text-uppercase" style={{"fontFamily": "monospace", "color": shouldHighlight ? "red" : "#6c757d", "textDecoration": shouldStrikethrough && "line-through"}} tabIndex={-1}>{inference.title}</a>
     </OverlayTrigger>
   }}</HashParamsContext.Consumer> ;
 };
