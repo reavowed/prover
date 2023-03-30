48b399a80070fe08a0cbaa08762b5a93920b8e76
Iain Monro <iamonro@gmail.com>
2023-03-30 14:58:11 +0100

    Fix theorem filtering in ChapterWithContext

--- src/main/scala/net/prover/entries/ChapterWithContext.scala
+++ src/main/scala/net/prover/entries/ChapterWithContext.scala
@@ -3,7 +3,7 @@ package net.prover.entries
 import net.prover.books.model.Book
 import net.prover.controllers.OptionWithResponseExceptionOps
 import net.prover.model.definitions.Definitions
-import net.prover.model.entries.ChapterEntry
+import net.prover.model.entries.{ChapterEntry, Theorem}
 import net.prover.model.{Chapter, Inference}
 
 import scala.reflect.{ClassTag, classTag}
@@ -22,7 +22,7 @@ case class ChapterWithContext(
 
   def entriesWithContexts: Seq[EntryWithContext] = chapter.entriesWithKeys.listWithKeys.map(getEntry[ChapterEntry])
   def inferencesWithContexts: Seq[TypedEntryWithContext[Inference.Entry]] = entriesWithContexts.ofType[TypedEntryWithContext[Inference.Entry]]
-  def theoremsWithContexts: Seq[TheoremWithContext] = entriesWithContexts.ofType[TheoremWithContext]
+  def theoremsWithContexts: Seq[TheoremWithContext] = entriesWithContexts.filter(_.entry.isInstanceOf[Theorem]).map(_.asInstanceOf[TheoremWithContext])
 
   def getEntry[T <: ChapterEntry : ClassTag](entryKey: String): Try[TypedEntryWithContext[T]] = {
     chapter.entriesWithKeys.listWithKeys.find(_._2 == entryKey)
